515e6c960261787147a15c8cd012e35b67c85a59
markshawn2020 <shawninjuly@gmail.com>
2023-09-19 09:09:35 +0800

    suppressed controller

--- app/room/layers/03-controllers.layer.tsx
+++ app/room/layers/03-controllers.layer.tsx
@@ -16,17 +16,17 @@ export default function ControllersLayer() {
       <div className={"right-8 bottom-8 hidden"}>
         <Shoot
           onPressing={(t) => {}}
-          onFinish={(t) => {
-            featherManager.current.feathers.forEach((feather) => {
-              const { x, y } = toUserPos(feather, k, n)
-
-              if (y > 0.9 - t / 10) {
-                if (Math.abs(playerX - x) < 0.1) {
-                  feather.invert = true
-                }
-              }
-            })
-          }}
+          // onFinish={(t) => {
+          //   featherManager.current.feathers.forEach((feather) => {
+          //     const { x, y } = toUserPos(feather, k, n)
+          //
+          //     if (y > 0.9 - t / 10) {
+          //       if (Math.abs(playerX - x) < 0.1) {
+          //         feather.invert = true
+          //       }
+          //     }
+          //   })
+          // }}
         />
       </div>
     </Layer>
